0427987af99028497813b264d0cabd1e3f9c8cd9
James Privett <jamesprivett29@gmail.com>
2021-03-26 15:35:26 +0000

    Displaying score

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -82,7 +82,7 @@ const App = () => {
           null
       }
 
-      { !gameOver ? <p className='score'>Score:</p> : null}
+      { !gameOver ? <p className='score'>Score: {score}</p> : null}
 
       { loading && <p>Loading Questions...</p>}
 
